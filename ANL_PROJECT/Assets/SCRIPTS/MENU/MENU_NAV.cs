c20e126d48e5d21fa7cd7521b7daf4d1d00b7806
DjAnjie <angiegaming817@gmail.com>
2022-05-11 13:21:42 +0200

    Finalisations of Demo

--- ANL_PROJECT/Assets/SCRIPTS/MENU/MENU_NAV.cs
+++ ANL_PROJECT/Assets/SCRIPTS/MENU/MENU_NAV.cs
@@ -2,18 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MENU_NAV : MonoBehaviour
 {
+    [SerializeField] Canvas settings;
+    [SerializeField] Canvas mainMenu;
+
+    [SerializeField] Canvas MainGameUI;
+
     public void MAIN_GAME() {
         SceneManager.LoadScene("MAIN_LEVEL");
     }
 
     public void SETTINGS() {
-        SceneManager.LoadScene("SETTINGS");
+        if (settings.enabled)
+        {
+            settings.enabled = false;
+            mainMenu.enabled = true;
+        }
+        else
+        {
+            settings.enabled = true;
+            mainMenu.enabled = false;
+        }
     }
+
 
     public void QUIT() {
         Application.Quit();
+    }
+
+    public void PAUSE() {
+        if(Time.timeScale == 1f) {
+            Time.timeScale = 0f;
+            MainGameUI.enabled = false;
+            MainGameUI.gameObject.SetActive(false);
+            mainMenu.enabled = true;
+            AudioListener.pause = true;
+        } else {
+            Time.timeScale = 1f ;
+            AudioListener.pause = false;
+            MainGameUI.enabled = true;
+            MainGameUI.gameObject.SetActive(true);
+            mainMenu.enabled = false;
+        }
     }
 }
---
+++ ANL_PROJECT/Assets/SCRIPTS/POINT_CLICK/Object_Click.cs
@@ -0,0 +1,213 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class Object_Click : MonoBehaviour
+{
+    [SerializeField] Text popupText,holdingItem;
+    [SerializeField] Button gBush,gBottle;
+
+    [SerializeField] public Sprite rockSprite,glassSprite,mapBottle;
+    [SerializeField] public Sprite mapSprite;
+
+    private string holding = "";
+
+    public bool isBottleDestroyed = false;
+
+    public static string[] inventory = new string[3];
+
+    [SerializeField] Button ButtonOne, ButtonTwo,ButtonThree,backpackButton;
+
+
+    [SerializeField] Image backgroundImage;
+
+    void Start() {
+        ButtonOne.gameObject.SetActive(false);
+        ButtonTwo.gameObject.SetActive(false);
+        ButtonThree.gameObject.SetActive(false);
+    }
+
+    public void OpenBackpack()
+    {
+        if (backgroundImage.enabled)
+        {
+            backgroundImage.enabled = false;
+            ButtonThree.gameObject.SetActive(false);
+            if (inventory[0] != null)
+            {
+                ButtonOne.gameObject.SetActive(false);
+            }
+            if (inventory[1] != null)
+            {
+                ButtonTwo.gameObject.SetActive(false);
+            }
+        }
+        else
+        {
+            backgroundImage.enabled = true;
+            ButtonThree.gameObject.SetActive(true);
+            if(inventory[0] != null) {
+                ButtonOne.gameObject.SetActive(true);
+                ButtonOne.gameObject.SetActive(true);
+            }
+            if (inventory[1] != null) {
+                ButtonTwo.gameObject.SetActive(true);
+            } 
+            
+        }
+
+    }
+    public void bushClicked() {
+            gBush.gameObject.SetActive(false);
+            if(popupText.enabled) {
+                popupText.text = "Item Found: Rock";
+            } else {
+                popupText.enabled = true;
+                popupText.text = "Item Found: Rock";
+            }
+            if(inventory[0] != "Rock")  {
+                ButtonOne.gameObject.name = inventory[0];
+            }
+            StartCoroutine(removeText("Rock"));
+            }
+
+    IEnumerator removeText(string item) {
+        if(item == "Unhold your item to pick up the map") {
+            popupText.text = "Unhold your item to pick up the map";
+        }
+        if(inventory[0] == null) {
+            inventory[0] = item;
+            ButtonOne.gameObject.name = inventory[0];
+            if(ButtonOne.gameObject.name == item) {
+                ButtonOne.image.sprite =  rockSprite;
+            } else {
+                ButtonOne.image.sprite = glassSprite;
+            }
+        } else if(inventory[1] == null) {
+            inventory[1] = item;
+            ButtonTwo.gameObject.name = inventory[1];
+            if (ButtonTwo.gameObject.name == item)
+            {
+                ButtonTwo.image.sprite = glassSprite;
+            }
+            else
+            {
+                ButtonTwo.image.sprite = rockSprite;
+            }
+        } else if(inventory[2] == null) {
+            inventory[2] = item;
+        }
+        yield return new WaitForSecondsRealtime(2);
+        popupText.enabled = false;
+    }
+
+    IEnumerator removeTextTwo() {
+        popupText.enabled = true;
+        popupText.text = "You need to be holding the rock to smash the bottles";
+        yield return new WaitForSecondsRealtime(2);
+        popupText.enabled = false;
+    }
+
+    public void Inventory() {
+        string name = EventSystem.current.currentSelectedGameObject.name;
+        if(inventory[0] != null && name == inventory[0]) {
+            ButtonOne.gameObject.SetActive(false);
+            holding = inventory[0];
+        } else if (inventory[1] != null && name == inventory[1]) {
+            ButtonTwo.gameObject.SetActive(false);
+            holding = inventory[1];
+        }
+    }
+
+    void Update() {
+        if(holding == "") {
+            holdingItem.text = "Currently Holding: Nothing";
+            if(!backgroundImage.enabled) {
+                ButtonOne.gameObject.SetActive(false);
+                ButtonTwo.gameObject.SetActive(false);
+            } else {
+                if(inventory[0] != null) {
+                    ButtonOne.gameObject.SetActive(true);
+                }
+                if(inventory[1] != null) {
+                    ButtonTwo.gameObject.SetActive(true);
+                }
+            }
+
+        } else if(holding == "Rock"){ 
+            holdingItem.text = "Currently Holding: Rock";
+            if (inventory[1] != null && backgroundImage.enabled)
+            {
+                ButtonTwo.gameObject.SetActive(true);
+            }
+        } else if (holding == "Glass") {
+            holdingItem.text = "Currently Holding: Glass";
+            if (inventory[0] != null && backgroundImage.enabled)
+            {
+                ButtonOne.gameObject.SetActive(true);
+            }
+        }
+    }
+    public void DestroyBottle() {
+        if(holding != "Rock") {
+            if(gBottle.image.sprite.name != "Map") {
+                StartCoroutine("removeTextTwo");
+            }
+        } else if(holding == "Rock" && !isBottleDestroyed) {
+            popupText.enabled=true;
+            isBottleDestroyed = true;
+            popupText.text = "Item Found: Glass";
+            if(inventory[0] != null) {
+                if (inventory[1] == null) {
+                    ButtonTwo.gameObject.name = inventory[1];
+                } else {
+                    ButtonTwo.gameObject.name = inventory[2];
+                }
+            } 
+            StartCoroutine(removeText("Glass"));
+            gBottle.image.sprite = mapSprite;
+        }
+
+        if (holding == "" && gBottle.image.sprite == mapSprite)
+        {
+            Debug.Log("You Win");
+            gBottle.image.enabled = false;
+            for (int i = 0; i < inventory.Length; i++)
+            {
+                inventory[i] = null;
+            }
+
+            SceneManager.LoadScene("MAIN_MENU");
+        }
+        else if (holding != "" && gBottle.image.sprite== mapSprite)
+        {
+            popupText.enabled = true;
+            StartCoroutine(removeText("Unhold your item to pick up the map"));
+        }
+    }
+
+    public void clearItemHolding() {
+        if(holding == "Rock") {
+            if(inventory[0] == holding) {
+                ButtonOne.gameObject.SetActive(true);
+            } else if (inventory[1]== holding) {
+                ButtonTwo.gameObject.SetActive(true);
+            }
+
+        } else if (holding == "Glass") {
+            if (inventory[0] == holding)
+            {
+                ButtonOne.gameObject.SetActive(true);
+            }
+            else if (inventory[1] == holding)
+            {
+                ButtonTwo.gameObject.SetActive(true);
+        }
+    }
+        holding = "";
+    }
+}
