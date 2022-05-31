17d4c62622c3bd21471bd42ddcd0b40b17b9e7ad
DjAnjie <angiegaming817@gmail.com>
2022-05-31 17:40:30 +0200

    Other Refinements

--- ANL_PROJECT/Assets/SCRIPTS/UI/SPRITE_CHANGER.cs
+++ ANL_PROJECT/Assets/SCRIPTS/UI/SPRITE_CHANGER.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 
 
-public class SPRITE_CHANGER : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class SPRITE_CHANGER : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler,IPointerClickHandler
 {
     [SerializeField] Sprite normalSprite;
     [SerializeField] Sprite hoverSprite;
+
+    [SerializeField] Sprite openSprite;
     [SerializeField] Button button;
 
     [SerializeField] float minX,minY,minZ;
@@ -17,19 +20,46 @@ public class SPRITE_CHANGER : MonoBehaviour, IPointerEnterHandler, IPointerExitH
 
     public int delay = 200;
 
+    private bool isOpen = false;
+
+    private string SceneName;
+
+    void Start() {
+        SceneName  = SceneManager.GetActiveScene().name;
+    }
+
     public void OnPointerEnter(PointerEventData eventData) {
-        button.image.sprite = hoverSprite;
+
+        if(!isOpen || SceneName == "MAIN_MENU") {
+            button.image.sprite = hoverSprite;
+        }
+        
         
         button.transform.localScale = new Vector3(minX, minY, minZ);
         button.transform.localScale = new Vector3(midX, midY, midZ);
         button.transform.localScale = new Vector3(maxX, maxY, maxZ);
     }
     public void OnPointerExit(PointerEventData eventData) {
-        button.image.sprite = normalSprite;
+        if(!isOpen || SceneName == "MAIN_MENU")
+        {
+            button.image.sprite = normalSprite;
+        }
 
         button.transform.localScale = new Vector3(maxX, maxY, maxZ);    
         button.transform.localScale = new Vector3(midX, midY, midZ);
         button.transform.localScale = new Vector3(minX, minY, minZ);
 
+    }
+    public void OnPointerClick(PointerEventData eventData) {
+        if(isOpen && SceneName != "MAIN_MENU") {
+            button.image.sprite = normalSprite;
+            isOpen = false;
+        } else if(!isOpen && SceneName != "MAIN_MENU") {
+            button.image.sprite = openSprite;
+            isOpen = true;
+            button.transform.localScale = new Vector3(maxX, maxY, maxZ);
+            button.transform.localScale = new Vector3(midX, midY, midZ);
+            button.transform.localScale = new Vector3(minX, minY, minZ);
+        }
     }
 }
